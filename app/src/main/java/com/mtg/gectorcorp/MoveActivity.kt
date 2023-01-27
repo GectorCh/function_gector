0684d58665dad45e15f3c59ef18927bf27411799
gectorch <gectorch@gmail.com>
2023-01-27 08:05:43 +0700

    :)

---
+++ app/src/main/java/com/mtg/gectorcorp/MoveActivity.kt
@@ -0,0 +1,11 @@
+package com.mtg.gectorcorp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
